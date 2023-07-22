722bdc1ee8ffb3fe41bab297b783423750fe8a65
Aleksandr Trybrat <alexandr.t1808@gmail.com>
2023-07-22 14:35:25 +0300

    electron

---
+++ src/electron.js
@@ -0,0 +1,83 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+app.on('ready', () => {
+  const mainWindow = new BrowserWindow({
+    show: false,
+    // width: 980,
+    // height: 820,
+    autoHideMenuBar: true,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true,
+    },
+    // подключение иконки
+    icon: path.join(__dirname, '../img/favicon.png')
+  });
+
+  mainWindow.maximize();
+
+  mainWindow.on('ready-to-show', () => {
+    mainWindow.show();
+  });
+
+
+  mainWindow.webContents.on('dom-ready', () => {
+    mainWindow.webContents.executeJavaScript(`
+      const script = document.createElement('script');
+      script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js';
+      script.onload = function() {
+        const slickScript = document.createElement('script');
+        slickScript.src = 'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js';
+        slickScript.onload = function() {
+          $('.slider').slick({
+            slidesToShow: 6,
+            slidesToScroll: 6,
+            autoplay: false,
+            autoplaySpeed: 3000,
+            prevArrow: '<button type="button" class="slick-prev">Previous</button>',
+            nextArrow: '<button type="button" class="slick-next">Next</button>',
+            responsive: [
+              {
+                breakpoint: 1024,
+                settings: {
+                  slidesToShow: 3,
+                  slidesToScroll: 3,
+                  infinite: true,
+                }
+              },
+              {
+                breakpoint: 600,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 2
+                }
+              },
+              {
+                breakpoint: 480,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 1
+                }
+              }
+          ]
+          });
+        };
+        document.head.append(slickScript);
+      };
+      document.head.append(script);
+    `);
+  });
+
+  mainWindow.loadURL('https://aleksandrtrybrat.github.io/clone_netflix/');
+
+});
+
+app.on('activate', function () {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
